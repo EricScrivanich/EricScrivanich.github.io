3265e026d08a8088ebc7d9df7ee7cf098e61a871
EricScrivanich <ersc6591@colorado.edu>
2023-11-07 10:42:44 -0700

    push

--- wa/wa10/wa10.js
+++ wa/wa10/wa10.js
@@ -25,6 +25,28 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 // event listener
 
 randomize.addEventListener('click',result);
+dropdownList.addEventListener('change',changeColor);
+
+function changeColor() {
+  let emotion = dropdownList.value;
+  switch (emotion) {
+    case "Scared":
+    dropdownList.style.backgroundColor = "red";
+    break;
+
+    case "Happy":
+    dropdownList.style.backgroundColor = "yellow";
+    break;
+
+    case "Bored":
+    dropdownList.style.backgroundColor = "lightgray";
+    break;
+
+
+  }
+
+
+}
 
 
 //method
@@ -36,6 +58,8 @@ function result(){
 
   if (emotion === "Scared")
   {
+  
+  
     storyText = `In the dead of night, :person's sleep was a distant memory. The walls of :place seemed to close in, suffocating in their silence. The only companion was a :object, a :adjective family heirloom that always seemed to hum with a strange energy after dark. It was a night like any other until the :object began to vibrate, its usual hum escalating into a frantic buzz.
 
     :person clutched the :object, the familiar coldness of its surface now a source of scant comfort. Shadows played across the room, puppeteered by the moonlight that filtered through the curtains. But one shadow was darker than the rest, a patch of night that seemed to suck in the light around it.
@@ -45,6 +69,7 @@ function result(){
   }
   if (emotion === "Happy")
   {
+    
     storyText =`The sun was high over :place, its rays like a benediction over the eager treasure hunter. :person's eyes were alight with the fire of long-held dreams, their hands steady on the metal detector, a tool that sang with potential. It was a melody of possibility that had whispered to them since childhood, a siren song of discovery.
 
     With each chirp of the detector, :person's heart leapt, the device a compass leading them through the familiar yet mysterious expanse of :place. This was no mere hunt; it was a pilgrimage to fulfill a yearning that had nestled in :person's soul since they were young. The detector's call crescendoed, a symphony that crescendoed as it announced a presence beneath the soft, yielding earth.
@@ -54,6 +79,7 @@ function result(){
   }
   if (emotion === "Bored")
   {
+ 
     storyText =`:person sat at :place, "The :object"— a :adjective book—lying forgotten in their lap. The words had blurred into a monotonous rhythm, each sentence as dull as the one before. :place, usually a haven, now felt like a prison, "The :object", a symbol of the day's drudgery.
 
     The clock ticked, a slow, torturous sound that seemed to mock :person's boredom. They picked up the :object again, hoping to find some escape within its :adjective pages. But the words were just words, lifeless and flat.
