8cbee7a6b621ae7fd717730aded62233c2d4209f
EricScrivanich <ersc6591@colorado.edu>
2023-11-30 13:57:19 -0700

    push

---
+++ wa/WA13/wa13.js
@@ -0,0 +1,146 @@
+const people = 
+ [
+    {
+      "name" : "Sam",
+      "category" : "Tech",
+      "position" : "Manager",
+      "salary" : 40000,
+      "raise" : true,
+    },
+    {
+        "name" : "Mary",
+        "category" : "Finance",
+        "position" : "Trainee",
+        "salary" : 18500,
+        "raise" : true,
+      },
+
+      {
+        "name" : "Bill",
+        "category" : "HR",
+        "position" : "Executive",
+        "salary" : 21200,
+        "raise" : false
+      }
+
+]
+
+
+console.log("Question 1");
+console.log(people);
+
+const company = 
+     
+          {
+            "company name" : "Tech Stars",
+            "website" : "www.techstars.site",
+            "employees" : [
+            {
+            "name" : "Sam",
+            "category" : "Tech",
+            "position" : "Manager",
+            "salary" : 40000,
+            "raise" : true,
+            },
+            {
+            "name" : "Mary",
+            "category" : "Finance",
+            "position" : "Trainee",
+            "salary" : 18500,
+            "raise" : true,
+            },
+              
+            {
+                "name" : "Bill",
+                "category" : "HR",
+                "position" : "Executive",
+                "salary" : 21200,
+                "raise" : false
+            }
+
+            ]
+          }
+    
+    
+    
+    console.log("Question 2");
+    console.log(company);
+
+
+const companyUpdate = 
+         
+              {
+                "company name" : "Tech Stars",
+                "website" : "www.techstars.site",
+                employees : [
+                {
+                "name" : "Sam",
+                "category" : "Tech",
+                "position" : "Manager",
+                "salary" : 40000,
+                "raise" : true,
+                },
+                {
+                "name" : "Mary",
+                "category" : "Finance",
+                "position" : "Trainee",
+                "salary" : 18500,
+                "raise" : true,
+                },
+                  
+                {
+                    "name" : "Bill",
+                    "category" : "HR",
+                    "position" : "Executive",
+                    "salary" : 21200,
+                    "raise" : false,
+                },
+                {
+                    "name" : "Anna",
+                    "category" : "Tech",
+                    "position" : "Executive",
+                    "salary" : 25600,
+                    "raise" : false
+                }
+
+    
+                ]
+              }
+
+          
+        
+        
+       
+        
+        console.log("Question 3");
+        console.log(companyUpdate);
+
+        let totalSalary = 0;
+        for(i=0;i<companyUpdate.employees.length; i++)
+        {
+            totalSalary += companyUpdate.employees[i]['salary'];
+            
+        }
+        console.log("Question 4");
+        console.log(totalSalary);
+
+        
+        for(i=0; i<companyUpdate.employees.length; i++)
+        {
+            if(companyUpdate.employees[i]['raise'] == true)
+            {
+                companyUpdate.employees[i]['salary'] += (companyUpdate.employees[i]['salary'] * .1);
+                companyUpdate.employees[i]['raise'] = false;
+
+            }
+            
+        }
+        console.log("Question 5");
+        console.log(companyUpdate);
+
+
+
+     
+
+    
+
