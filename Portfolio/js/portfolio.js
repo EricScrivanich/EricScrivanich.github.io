8c70f3a72c0e7dfce20d60f5f6d3cef9695d992c
EricScrivanich <ersc6591@colorado.edu>
2024-04-01 10:58:10 -0600

    push

--- Portfolio/js/portfolio.js
+++ Portfolio/js/portfolio.js
@@ -1,14 +1,40 @@
 const originalTitleWidth = 450;
 
 
-const headerImage = document.querySelector(".header-image");
+
+
+if (document.querySelector(".header-image") != null) 
+{
+let headerImage = document.querySelector(".header-image");
+headerImage.style.width = `${originalTitleWidth}px`;
+
+window.addEventListener("scroll", function () {
+  const scrollPosition = window.scrollY;
+  const minImageWidth = 260; // Minimum width the image can shrink to
+  const shrinkStart = 50; // Scroll position where the image starts shrinking
+  const shrinkRate = 2; // Pixels reduced per scroll pixel
+
+  // Calculate the new width based on the scroll position
+  let shrinkAmount = Math.max(0, (scrollPosition - shrinkStart) * shrinkRate);
+  let newWidth = Math.max(minImageWidth, originalTitleWidth - shrinkAmount);
+
+  // Calculate the amount to translate the image on the X-axis
+  let translateX = (originalTitleWidth - newWidth) / 2;
+
+  // Apply the new width and translation
+  headerImage.style.width = `${newWidth}px`;
+  // headerImage.style.transform = `translateX(${translateX}px)`;
+
+  finalWidth = newWidth; // Keep track of the final width
+});
+}
 
 
 
 let finalWidth = originalTitleWidth;
 
 
-headerImage.style.width = `${originalTitleWidth}px`;
+
 
 
 
@@ -63,6 +89,76 @@ document.addEventListener("DOMContentLoaded", function () {
   DoCountdown(); // Start the countdown for automatic transition
 });
 
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let currentImageIndex = 0;
+//   const galleryImages = [
+//     "../img/TFCover.jpg",
+//     "../img/TF1.jpg",
+//     "../img/TF2.jpg",
+//     "../img/TF3.jpg",
+//     "../img/TF4.jpg",
+//     "../img/TF5.jpg",
+//     "../img/TF6.jpg",
+//     "../img/TF7.jpg",
+//     "../img/TF8.jpg",
+//     "../img/TF9.jpg",
+//     // Add more image paths as needed
+//   ];
+//   const imageContainer = document.getElementById("currentDisplayedImage");
+//   const nextButton = document.getElementById("next");
+//   const prevButton = document.getElementById("prev");
+
+//   function transitionImage(inOut = true) {
+//     const opacity = inOut ? 1 : 0; 
+//     const rotation = inOut ? "0deg" : "360deg"; 
+
+//     imageContainer.style.opacity = opacity;
+//     imageContainer.style.transform = `rotate(${rotation})`;
+//   }
+
+//   function showImage(index) {
+//     const img = document.createElement("img");
+//     img.src = galleryImages[index];
+//     img.alt = "Gallery Image";
+//     img.className = "gallery-image";
+
+   
+//     transitionImage(false);
+
+//     setTimeout(() => {
+     
+//       imageContainer.innerHTML = "";
+//       imageContainer.appendChild(img);
+
+//       // Transition in the new image
+//       transitionImage(true);
+//     }, 1300); 
+//   }
+
+//   function nextImage() {
+//     currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
+//     showImage(currentImageIndex);
+//   }
+
+//   function previousImage() {
+//     currentImageIndex =
+//       (currentImageIndex - 1 + galleryImages.length) % galleryImages.length;
+//     showImage(currentImageIndex);
+//   }
+
+//   nextButton.addEventListener("click", nextImage);
+//   prevButton.addEventListener("click", previousImage);
+
+
+//   showImage(currentImageIndex);
+// });
+
+
+
+
+
+
 // window.addEventListener("scroll", function () {
 //   const scrollPosition = window.scrollY;
 
@@ -89,23 +185,4 @@ document.addEventListener("DOMContentLoaded", function () {
 //   }
 // });
 
-window.addEventListener("scroll", function () {
-  const scrollPosition = window.scrollY;
-  const minImageWidth = 260; // Minimum width the image can shrink to
-  const shrinkStart = 50; // Scroll position where the image starts shrinking
-  const shrinkRate = 2; // Pixels reduced per scroll pixel
-  
 
-  // Calculate the new width based on the scroll position
-  let shrinkAmount = Math.max(0, (scrollPosition - shrinkStart) * shrinkRate);
-  let newWidth = Math.max(minImageWidth, originalTitleWidth - shrinkAmount);
-
-  // Calculate the amount to translate the image on the X-axis
-  let translateX = (originalTitleWidth - newWidth) / 2;
-
-  // Apply the new width and translation
-  headerImage.style.width = `${newWidth}px`;
-  // headerImage.style.transform = `translateX(${translateX}px)`;
-
-  finalWidth = newWidth; // Keep track of the final width
-});
